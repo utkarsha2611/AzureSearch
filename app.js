de1fcdccb6e1d3bb264f599653e89d46452b8bfd
geeky.coder <utkarsha2611@users.noreply.github.com>
2018-03-14 18:00:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-/*-----------------------------------------------------------------------------
-Roller is a dice rolling skill that's been optimized for speech. 
------------------------------------------------------------------------------*/
-
 require('dotenv-extended').load();
 var restify = require('restify');
 var builder = require('botbuilder');
@@ -22,13 +18,7 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-/**
- * Create your bot with a function to receive messages from the user.
- * - This function will be called anytime the users utterance isn't
- *   recognized.
- */
 var bot = new builder.UniversalBot(connector, function (session) {
-    // Just redirect to our 'HelpDialog'.
     session.replaceDialog('HelpDialog');
 });
 var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
@@ -37,10 +27,6 @@ bot.recognizer(recognizer);
 bot.dialog('HelpDialog', function (session) {
     var card = new builder.HeroCard(session)
         .title('Hey there! How can I help you today?');
-    //    .buttons([
-    //    builder.CardAction.imBack(session, 'roll some dice', 'Roll Dice'),
-    //  builder.CardAction.imBack(session, 'play craps', 'Play Craps')
-    //  ]);
     var msg = new builder.Message(session)
         .speak(speak(session, 'Hey there! How can I help you today?'))
         .addAttachment(card)
@@ -48,20 +34,17 @@ bot.dialog('HelpDialog', function (session) {
     session.send(msg).endDialog();
 }).triggerAction({ matches: /help/i });
 
-
 bot.dialog('nearest', function (session) {
     var msg = new builder.Message(session)
         .speak(speak(session, 'The nearest center is Bangsar'))
-       // .addAttachment(card)
         .inputHint(builder.InputHint.acceptingInput);
     session.send(msg).endDialog();
 }).triggerAction({ matches: 'nearest' });
-/** Helper function to wrap SSML stored in the prompts file with <speak/> tag. */
+
 function speak(session, prompt) {
     var localized = session.gettext(prompt);
     return ssml.speak(localized);
 }
-
 bot.dialog('subscription', function (session) {
     var msg = new builder.Message(session)
         .speak(speak(session, 'We will inform you via SMS when 1-Day Social Roam Pass has been successfully activated.'))
@@ -69,11 +52,9 @@ bot.dialog('subscription', function (session) {
     session.send(msg).endDialog();
 }).triggerAction({ matches: 'subscription' });
 
-
 bot.dialog('roampass', function (session) {
     var msg = new builder.Message(session)
         .speak(speak(session, '1-Day Social Roam Pass can be used across these listed operators network in 10 countries.'))
         .inputHint(builder.InputHint.acceptingInput);
     session.send(msg).endDialog();
-}).triggerAction({ matches: 'roampass' });
-
+}).triggerAction({ matches: 'roampass'});
