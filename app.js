7ce24adae7c06a294269d24400300b2a5145ddd9
geeky.coder <utkarsha2611@users.noreply.github.com>
2018-03-14 17:54:15 +0800

    Update app.js

--- app.js
+++ app.js
@@ -33,201 +33,6 @@ var bot = new builder.UniversalBot(connector, function (session) {
 });
 var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
 bot.recognizer(recognizer);
-/**
- * This dialog sets up a custom game for the bot to play.  It will 
- * ask the user how many sides they want the dice to have and then
- * how many should be rolled. Once it's built up the game structure
- * it will pass it to a seperate 'PlayGameDialog'.
- * 
- * We've added a triggerAction() to this dialog that lets a user say
- * something like "I'd like to roll some dice" to start the dialog.
- * We're using a RegEx to match the users input but we could just as 
- * easily use a LUIS intent.
- */
-bot.dialog('CreateGameDialog', [
-    function (session) {
-        // Initialize game structure.
-        // - dialogData gives us temporary storage of this data in between
-        //   turns with the user.
-        var game = session.dialogData.game = {
-            type: 'custom',
-            sides: null,
-            count: null,
-            turns: 0
-        };
-
-        /**
-         * Ask for the number of sides.
-         * 
-         * You can pass an array of choices to be matched. These will be shown as a
-         * numbered list by default.  This list will be shown as a numbered list which
-         * is what we want since we have so many options.
-         * 
-         * - value is what you want returned via 'results.response.entity' when selected.
-         * - action lets you customize the displayed label and for buttons what get sent when clicked.
-         * - synonyms let you specify alternate things to recognize for a choice.
-         */
-        var choices = [
-            { value: '4', action: { title: '4 Sides' }, synonyms: 'four|for|4 sided|4 sides' },
-            { value: '6', action: { title: '6 Sides' }, synonyms: 'six|sex|6 sided|6 sides' },
-            { value: '8', action: { title: '8 Sides' }, synonyms: 'eight|8 sided|8 sides' },
-            { value: '10', action: { title: '10 Sides' }, synonyms: 'ten|10 sided|10 sides' },
-            { value: '12', action: { title: '12 Sides' }, synonyms: 'twelve|12 sided|12 sides' },
-            { value: '20', action: { title: '20 Sides' }, synonyms: 'twenty|20 sided|20 sides' }
-        ];
-        builder.Prompts.choice(session, 'choose_sides', choices, {
-            speak: speak(session, 'choose_sides_ssml')
-        });
-    },
-    function (session, results) {
-        // Store users input
-        // - The response comes back as a find result with index & entity value matched.
-        var game = session.dialogData.game;
-        game.sides = Number(results.response.entity);
-
-        /**
-         * Ask for number of dice.
-         * 
-         * - We can use gettext() to format a string using a template stored in our
-         *   localized prompts file.
-         * - The number prompt lets us pass additional options to say we only want
-         *   integers back and what's the min & max value that's allowed.
-         */
-        var prompt = session.gettext('choose_count', game.sides);
-        builder.Prompts.number(session, prompt, {
-            speak: speak(session, 'choose_count_ssml'),
-            minValue: 1,
-            maxValue: 100,
-            integerOnly: true
-        });
-    },
-    function (session, results) {
-        // Store users input
-        // - The response is already a number.
-        var game = session.dialogData.game;
-        game.count = results.response;
-
-        /**
-         * Play the game we just created.
-         * 
-         * We can use replaceDialog() to end the current dialog and start a new
-         * one in its place. We can pass arguments to dialogs so we'll pass the
-         * 'PlayGameDialog' the game we created.
-         */
-        session.replaceDialog('PlayGameDialog', { game: game });
-    }
-]).triggerAction({
-    matches: [
-        /(roll|role|throw|shoot).*(dice|die|dye|bones)/i,
-        /new game/i
-    ]
-});
-
-/**
- * This dialog is our main game loop. We'll store the game structure in
- * session.conversationData so that should the user say "roll again" we
- * can just re-roll the same set of dice again.
- */
-bot.dialog('PlayGameDialog', function (session, args) {
-    // Get current or new game structure.
-    var game = args.game || session.conversationData.game;
-    if (game) {
-        // Generate rolls
-        var total = 0;
-        var rolls = [];
-        for (var i = 0; i < game.count; i++) {
-            var roll = Math.floor(Math.random() * game.sides) + 1;
-            if (roll > game.sides) {
-                // Accounts for 1 in a million chance random() generated a 1.0
-                roll = game.sides;
-            }
-            total += roll;
-            rolls.push(roll);
-        }
-
-        // Format roll results
-        var results = '';
-        var multiLine = rolls.length > 5;
-        for (var i = 0; i < rolls.length; i++) {
-            if (i > 0) {
-                results += ' . ';
-            }
-            results += rolls[i];
-        }
-
-        // Render results using a card
-        var card = new builder.HeroCard(session)
-            .subtitle(game.count > 1 ? 'card_subtitle_plural' : 'card_subtitle_singular', game)
-            .buttons([
-                builder.CardAction.imBack(session, 'roll again', 'Roll Again'),
-                builder.CardAction.imBack(session, 'new game', 'New Game')
-            ]);
-        if (multiLine) {
-            //card.title('card_title').text('\n\n' + results + '\n\n');
-            card.text(results);
-        } else {
-            card.title(results);
-        }
-        var msg = new builder.Message(session).addAttachment(card);
-
-        // Determine bots reaction for speech purposes
-        var reaction = 'normal';
-        var min = game.count;
-        var max = game.count * game.sides;
-        var score = total / max;
-        if (score === 1.0) {
-            reaction = 'best';
-        } else if (score === 0) {
-            reaction = 'worst';
-        } else if (score <= 0.3) {
-            reaction = 'bad';
-        } else if (score >= 0.8) {
-            reaction = 'good';
-        }
-
-        // Check for special craps rolls
-        if (game.type === 'craps') {
-            switch (total) {
-                case 2:
-                case 3:
-                case 12:
-                    reaction = 'craps_lose';
-                    break;
-                case 7:
-                    reaction = 'craps_seven';
-                    break;
-                case 11:
-                    reaction = 'craps_eleven';
-                    break;
-                default:
-                    reaction = 'craps_retry';
-                    break;
-            }
-        }
-
-        // Build up spoken response
-        var spoken = '';
-        if (game.turn === 0) {
-            spoken += session.gettext('start_' + game.type + '_game_ssml') + ' ';
-        }
-        spoken += session.gettext(reaction + '_roll_reaction_ssml');
-        msg.speak(ssml.speak(spoken));
-
-        // Incrment number of turns and store game to roll again
-        game.turn++;
-        session.conversationData.game = game;
-
-        /**
-         * Send card and bots reaction to user. 
-         */
-        msg.inputHint(builder.InputHint.acceptingInput);
-        session.send(msg).endDialog();
-    } else {
-        // User started session with "roll again" so let's just send them to
-        // the 'CreateGameDialog'
-        session.replaceDialog('CreateGameDialog');
-    }
-}).triggerAction({ matches: /(roll|role|throw|shoot) again/i });
 
 bot.dialog('HelpDialog', function (session) {
     var card = new builder.HeroCard(session)
@@ -256,3 +61,19 @@ function speak(session, prompt) {
     var localized = session.gettext(prompt);
     return ssml.speak(localized);
 }
+
+bot.dialog('subscription', function (session) {
+    var msg = new builder.Message(session)
+        .speak(speak(session, 'We will inform you via SMS when 1-Day Social Roam Pass has been successfully activated.'))
+        .inputHint(builder.InputHint.acceptingInput);
+    session.send(msg).endDialog();
+}).triggerAction({ matches: 'subscription' });
+
+
+bot.dialog('roampass', function (session) {
+    var msg = new builder.Message(session)
+        .speak(speak(session, '1-Day Social Roam Pass can be used across these listed operators network in 10 countries.'))
+        .inputHint(builder.InputHint.acceptingInput);
+    session.send(msg).endDialog();
+}).triggerAction({ matches: 'roampass' });
+
